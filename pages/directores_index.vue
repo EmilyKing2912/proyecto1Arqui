1add0ace9244d05bdf6ac69f04116af05db7ea6d
EmilyKing2912 <82293890+EmilyKing2912@users.noreply.github.com>
2025-11-18 20:58:48 -0600

    Update directores_index.vue

--- pages/directores_index.vue
+++ pages/directores_index.vue
@@ -15,9 +15,11 @@
     <button class="btn">🏛️ Estudios</button>
   </NuxtLink>
   
-   <NuxtLink to="/carrito_index">
-    <button class="btn"> 🛒 Carrito</button>
-  </NuxtLink>
+<NuxtLink to="#">
+  <button class="btn" onclick="window.Snipcart.api.theme.cart.open()">
+    🛒 Carrito
+  </button>
+</NuxtLink>
 </div>
 
 
